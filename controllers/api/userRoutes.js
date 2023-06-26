3bfe38260b676d7b68e703047f8a347a3cd7e569
JimmyPedone <>
2023-06-26 18:18:40 -0400

    homeRoute edits

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,22 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 //TO DO: post route to create a new user
+router.post('/profile', async (req, res) => {
+  try {
+    const userData = await User.create(req.body)
+   
+      // Specify attributes so that we can render user name on the page
+      
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
 
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 //TO DO: post route to create a new session for user with login
 
 //TO DO: post route to end session with logout
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,6 +7,14 @@ router.get("/", async (req, res) => {
   try {
     // TO DO: specify attributes so that we can
     // render recipe name, recipe detail
+    const recipeName = await Recipe.findAll(
+      {
+
+      attributes: ['name', 'detail'],
+
+      },
+    );
+
     res.render("homepage");
   } catch (err) {
     res.status(500).json(err);
@@ -14,15 +22,80 @@ router.get("/", async (req, res) => {
 });
 
 // TO DO: create route for user profile.
-// Specify attributes so that we can render
-// user name on the page
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id,
+    {
+      attributes: {
+        exclude: ['password']
+      },
+        include: [{ model: Recipe }]
+    });
+
+    const user = userData.get({plain: true})
+
+    console.log(user)
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    })
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // TO DO: create route for recipe by id.
+
 // Specify attributes so that we can render
 // receipt name, recipe detail, recipe author, recipe date, and associated comments
 
 // TO DO: create route for login
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password'});
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 // TO DO: create route to sign up
+router.post('/signup', async (req, res) => {
+// what do I do here lol
+});
+  
+
 
 module.exports = router;
