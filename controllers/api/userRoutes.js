1240209114e0c8d824895655c4d962d16b147d2b
JimmyPedone <>
2023-06-26 18:32:36 -0400

    oopsie, I added userRoutes code to homeRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -19,7 +19,47 @@ router.post('/profile', async (req, res) => {
   }
 });
 //TO DO: post route to create a new session for user with login
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password'});
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 //TO DO: post route to end session with logout
 
 module.exports = router;
