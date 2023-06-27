ad9bb250bbe6e3ef772aeedb56b09c576a554f15
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-06-27 16:49:19 -0400

    made corrections and set sync to false so we can see seeds populate and check routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,77 +7,81 @@ router.get("/", async (req, res) => {
   try {
     // TO DO: specify attributes so that we can
     // render recipe name, recipe detail
-    const recipeName = await Recipe.findAll(
-      {
+    const recipeData = await Recipe.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
 
-      attributes: ['name', 'detail'],
-
-      },
-    );
-
-    res.render("homepage");
+    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+    res.render("homepage", {
+      recipes,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // TO DO: create route for user profile.
-router.get('/profile', withAuth, async (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
   try {
-    const userData = await User.findByPk(req.session.user_id,
-    {
+    const userData = await User.findByPk(req.session.user_id, {
       attributes: {
-        exclude: ['password']
+        exclude: ["password"],
       },
-        include: [{ model: Recipe }]
+      include: [{ model: Recipe }],
     });
 
-    const user = userData.get({plain: true})
+    const user = userData.get({ plain: true });
 
-    console.log(user)
-    res.render('profile', {
+    console.log(user);
+    res.render("profile", {
       ...user,
-      logged_in: true
-    })
+      logged_in: true,
+    });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
 // TO DO: create route for recipe by id.
-router.get('/profile/recipe/:id', async (req, res) => {
+router.get("/profile/recipe/:id", async (req, res) => {
   try {
     const recipeData = await Recipe.findByPk(req.params.id, {
       includes: [
         {
           model: User,
-          attributes: ['name'] 
+          attributes: ["name"],
         },
         {
           model: Comment,
-          attributes: ['recipe_id', 'user_id']
+          attributes: ["recipe_id", "user_id"],
         },
-      ]
-    })
-    const recipe = recipeData.get({plain: true})
-    console.log(recipe)
+      ],
+    });
+    const recipe = recipeData.get({ plain: true });
+    console.log(recipe);
     if (!recipeData) {
-      res.status(404).json({message: 'No recipe found'})
+      res.status(404).json({ message: "No recipe found" });
     }
-    res.render('/recipe/:id', {
-      ...recipe
-    })
+    res.render("/recipe/:id", {
+      ...recipe,
+    });
   } catch (err) {
     res.status(400).json(err);
   }
-}) 
+});
 // Specify attributes so that we can render
 // receipt name, recipe detail, recipe author, recipe date, and associated comments
 
 // TO DO: create route for login
 router.get("/login", (req, res) => {
   try {
-    if (req.session.logged_in){
+    if (req.session.logged_in) {
       res.redirect("./homepage");
       return;
     }
@@ -91,11 +95,9 @@ router.get("/signup", (req, res) => {
   try {
     res.render("signup");
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 //do this
-  
-
 
 module.exports = router;
