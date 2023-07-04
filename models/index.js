3b357339cbf0467ff44225b5bb01a71887220c48
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-07-04 19:13:23 -0400

    add comments to index in models folder, fix a model association

--- models/index.js
+++ models/index.js
@@ -1,41 +1,51 @@
+// import models
 const User = require("./User");
 const Recipe = require("./Recipe");
 const Comment = require("./Comment");
 const RecipeTag = require("./RecipeTag");
 const Tag = require("./Tag");
 
+// model associations
+
+// User has many recipes
 User.hasMany(Recipe, {
   foreignKey: "user_id",
 });
 
+// Recipe belongs to User
 Recipe.belongsTo(User, {
   foreignKey: "user_id",
 });
 
+// User has many comments
 User.hasMany(Comment, {
   foreignKey: "user_id",
 });
 
+// Comment belongs to user
 Comment.belongsTo(User, {
   foreignKey: "user_id",
 });
 
+// Recipe has many Comments 
 Recipe.hasMany(Comment, {
   foreignKey: "recipe_id",
 });
 
+// Comment belongs to recipe
 Comment.belongsTo(Recipe, {
   foreignKey: "recipe_id",
 });
 
+// Recipe belongs to many tags through RecipeTag
 Recipe.belongsToMany(Tag, {
   through: RecipeTag,
 });
 
-Tag.belongsToMany(RecipeTag, {
+// Tag belongs to many recipes through RecipeTag
+Tag.belongsToMany(Recipe, {
   through: RecipeTag,
 });
 
-//test
-
+// export all classes
 module.exports = { User, Recipe, Comment, RecipeTag, Tag };
