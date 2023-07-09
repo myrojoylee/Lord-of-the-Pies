e12e31f91a97e6507841fc6be743b47719fe8958
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-07-08 23:00:05 -0400

    delete code for logout message as it is too clunky

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,9 +1,3 @@
-// selecting an element through DOM and setting it to variable
-const messageParent = document.querySelector(".message-parent");
-// Setting a time variable for message timeout
-let time = 5;
-let time2 = 1;
-
 // async function to add logout functionality
 const logout = async () => {
   const response = await fetch("/api/users/logout", {
@@ -12,50 +6,11 @@ const logout = async () => {
   });
 
   if (response.ok) {
-    // create a message with logout
-    // let timerInterval2 = setInterval(function() {
-    //   time2--;
-    //   if (time2 === 0) {
-    //     clearInterval(timerInterval2);
-    //   }
-    // }, 1000);
-    createLogoutMessage();
+    document.location.replace("/");
+    
   } else {
     alert(response.statusText);
   }
-};
-const createLogoutMessage = async () => {
-  const message = document.createElement("h2");
-  // message.textContent = "You are now logged out. To login again, click ";
-  message.id = "logout-message";
-  message.style.color = "black";
-  message.classList.add("title");
-
-  // const link = document.createElement("a");
-  // link.href = "/login";
-  // link.id = "login-link";
-  // link.textContent = "here.";
-  // link.style.color = "blue";
-
-  // append it to the parent element
-  messageParent.appendChild(message);
-  // messageParent.appendChild(link);
-  // console.log(messageParent);
-
-  // set a timer interval to have message time out
-  setInterval(function () {
-    if (time === 0) {
-      // clearInterval(timerInterval);
-      // message.remove();
-      // link.remove();
-      document.location.replace("/");
-
-      console.log(messageParent);
-    } else {
-      message.textContent = `You will be logged out in ${time} seconds.`;
-    }
-    time--;
-  }, 1000);
 };
 
 // event listener to add function to logout button
